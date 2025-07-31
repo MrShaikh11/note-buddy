ffe2853870635627ee8679db857af9494c5cd321
Maaz Shaikh <maaz.z.shaikh@gmail.com>
2025-07-31 19:03:52 +0530

    minor UI fixes

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="min-h-screen grid grid-cols-1 md:grid-cols-2">
-      <div className="relative flex flex-col justify-center px-6 sm:px-12 md:px-16 lg:px-24 py-8">
-        <div className="absolute top-6 left-6  items-center gap-2 hidden md:flex">
+    <div className="min-h-screen h-screen grid grid-cols-1 md:grid-cols-2 overflow-hidden">
+      <div className="relative flex flex-col justify-center items-center h-screen px-6 sm:px-12 md:px-16 lg:px-24 bg-white">
+        <div className="absolute top-6 left-6 items-center gap-2 hidden md:flex">
           <Image src="/logo.svg" alt="Logo" width={32} height={32} />
           <span className="text-2xl font-semibold text-[#232323]">HD</span>
         </div>
 
-        <div className="flex items-center justify-center space-x-2 mb-8  md:hidden">
+        <div className="flex items-center justify-center space-x-2 mb-8 md:hidden">
           <Image src="/logo.svg" alt="Logo" width={24} height={24} />
           <span className="text-xl font-bold text-[#232323]">HD</span>
         </div>
